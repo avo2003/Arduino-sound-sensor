f5c5d0a7591de41f5b740dd50136943fdce14a50
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:28:33 +0400

    Add files via upload

---
+++ sketch_jul01b.ino
@@ -0,0 +1,54 @@
+int micPin = A0;     
+int gndPin = A1;
+int powerPin = A2;
+int micValue1 = 0;  
+int micValue2 = 0; 
+
+
+void setup() {
+    Serial.begin(9600); 
+  pinMode(powerPin, OUTPUT);
+  pinMode(gndPin, OUTPUT);
+  pinMode(micPin, INPUT);
+  digitalWrite(gndPin,LOW);
+  delay(500);
+  digitalWrite(powerPin,HIGH);
+  Serial.begin(9600);  
+}
+
+void loop() {
+  micValue1 = analogRead(micPin);  
+  delay(10);
+  micValue2 = analogRead(micPin);
+  
+  if (micValue2-micValue1 > 2){
+     Serial.println(micValue2-micValue1);
+  delay(1000);
+  }
+  
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
