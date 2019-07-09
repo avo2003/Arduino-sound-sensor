e8ff9b7212dae33f15e1e097a25c7dde219536bf
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:55:56 +0400

    Add files via upload

---
+++ sketch_jul03a.ino
@@ -0,0 +1,20 @@
+const int soundpin=A2;
+const int threshold=50; 
+void setup() {
+Serial.begin(9600);
+
+pinMode(soundpin,INPUT);
+}
+void loop() {
+int soundsens=analogRead(soundpin);
+if (soundsens>=threshold) {
+ Serial.println(soundsens);
+  delay(1000);
+}
+
+else{
+ Serial.println("sxala");
+ delay(1000);
+}
+}
+
